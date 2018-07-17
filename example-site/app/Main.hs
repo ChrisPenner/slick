ab3e3c9b7ff9df083f1d77f5af12c1b32a12b537
Chris Penner <christopher.penner@gmail.com>
2018-07-17 16:32:30 -0600

    Add docs to example site

--- example-site/app/Main.hs
+++ example-site/app/Main.hs
@@ -18,7 +18,7 @@ import Data.Set as S
 import qualified Data.Text as T
 import Data.Text.Lens
 import Data.Time
-import Development.Shake hiding (Resource)
+import Development.Shake
 import Development.Shake.Classes
 import Development.Shake.FilePath
 import GHC.Generics (Generic)
@@ -29,7 +29,7 @@ main =
   shakeArgs shakeOptions {shakeVerbosity = Chatty} $
     -- Set up caches
    do
-    postCache <- jsonCache loadPost
+    postCache <- jsonCache' loadPost
     -- Require all the things we need to build the whole site
     "site" ~> need ["static", "posts", "dist/index.html"]
     -- Require all static assets
@@ -47,6 +47,7 @@ main =
      -- rule for actually building posts
     "dist/posts//*.html" %> buildPost postCache
 
+-- | Represents the template dependencies of the index page
 data IndexInfo = IndexInfo
   { posts :: [Post]
   } deriving (Generic, Show)
@@ -55,6 +56,7 @@ instance FromJSON IndexInfo
 
 instance ToJSON IndexInfo
 
+-- | A JSON serializable representation of a post's metadata
 data Post = Post
   { title :: String
   , author :: String
@@ -68,18 +70,31 @@ instance FromJSON Post
 
 instance ToJSON Post
 
+
+-- A simple wrapper data-type which implements 'ShakeValue'; 
+-- Used as a Shake Cache key to build a cache of post objects.
+newtype PostFilePath =
+  PostFilePath String
+  deriving (Show, Eq, Hashable, Binary, NFData)
+
+-- | Discover all available post source files
 postNames :: Action [FilePath]
 postNames = getDirectoryFiles "." ["site/posts//*.md"]
 
+-- | convert 'build' filepaths into source file filepaths
 destToSrc :: FilePath -> FilePath
 destToSrc p = "site" </> dropDirectory1 p
 
+-- | convert source filepaths into build filepaths
 srcToDest :: FilePath -> FilePath
 srcToDest p = "dist" </> dropDirectory1 p
 
+-- | convert a source file path into a URL
 srcToURL :: FilePath -> String
 srcToURL = ("/" ++) . dropDirectory1 . (-<.> ".html")
 
+-- | Given a post source-file's file path as a cache key, load the Post object
+-- for it. This is used with 'jsonCache' to provide post caching.
 loadPost :: PostFilePath -> Action Post
 loadPost (PostFilePath postPath) = do
   let srcPath = destToSrc postPath -<.> "md"
@@ -89,6 +104,7 @@ loadPost (PostFilePath postPath) = do
       withSrc = _Object . at "srcPath" ?~ String (T.pack srcPath)
   convert . withSrc . withURL $ postData
 
+-- | given a cache of posts this will build a table of contents
 buildIndex :: (PostFilePath -> Action Post) -> FilePath -> Action ()
 buildIndex postCache out = do
   posts <- postNames >>= traverse (postCache . PostFilePath)
@@ -97,11 +113,14 @@ buildIndex postCache out = do
       indexHTML = T.unpack $ substitute indexT (toJSON indexInfo)
   writeFile' out indexHTML
 
+-- | Find all post source files and tell shake to build the corresponding html
+-- pages.
 requirePosts :: Action ()
 requirePosts = do
   pNames <- postNames
   need ((\p -> srcToDest p -<.> "html") <$> pNames)
 
+-- Build an html file for a given post given a cache of posts.
 buildPost :: (PostFilePath -> Action Post) -> FilePath -> Action ()
 buildPost postCache out = do
   let srcPath = destToSrc out -<.> "md"
@@ -109,7 +128,3 @@ buildPost postCache out = do
   post <- postCache (PostFilePath srcPath)
   template <- compileTemplate' "site/templates/post.html"
   writeFile' out . T.unpack $ substitute template (toJSON post)
-
-newtype PostFilePath =
-  PostFilePath String
-  deriving (Show, Eq, Hashable, Binary, NFData)

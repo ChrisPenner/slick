9f40bdbcf5f7f959a1a1cb9666825072eadeab28
Sergey <sergey.bushnyak@sigrlami.eu>
2019-05-05 15:25:02 +0300

    refactor example site to the new structure

---
+++ example/app/Builder.hs
@@ -0,0 +1,122 @@
+{-# LANGUAGE CPP                       #-}
+{-# LANGUAGE DataKinds                 #-}
+{-# LANGUAGE DeriveGeneric             #-}
+{-# LANGUAGE DuplicateRecordFields     #-}
+{-# LANGUAGE NamedFieldPuns            #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE Rank2Types                #-}
+{-# LANGUAGE RecordWildCards           #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
+{-# LANGUAGE TemplateHaskell           #-}
+
+module Builder where
+
+import           Control.Applicative
+import           Control.Lens
+import           Control.Monad                (liftM, mzero)
+import           Data.Aeson                   as A
+import           Data.Aeson.Lens
+import           Data.Aeson.TH
+import           Data.Aeson.TH                (Options (..), defaultOptions,
+                                               deriveJSON)
+import           Data.Char
+import           Data.Function                (on)
+import           Data.List                    (intersperse, sortBy)
+import           Data.Map                     as M
+import           Data.Maybe
+import           Data.Monoid
+import           Data.Set                     as S
+import           Data.String.Utils
+import qualified Data.Text                    as T
+import           Data.Text.Lens
+import           Data.Time
+import           Development.Shake
+import           Development.Shake.Classes
+import           Development.Shake.FilePath
+import           Slick
+import           Slick.Pandoc
+import           System.Console.GetOpt
+import           System.Environment
+import           Text.Pandoc.Extensions
+import           Text.Pandoc.Options
+import           Text.Pandoc.Readers.Markdown (readMarkdown)
+import           Text.Pandoc.Writers.HTML     (writeHtml5String)
+
+import           Types
+
+------------------------------------------------------------------------------
+-- Builder related functions
+
+-- | Find all post source files and tell shake to build
+--   the corresponding html pages.
+requirePosts :: Action ()
+requirePosts = do
+  pNames <- postNames
+  need ((\p -> srcToDest p -<.> "html") <$> pNames)
+
+--------------------------------------------------------------------------------
+-- FilePaths loaders
+
+-- | Discover all available post source files
+postNames :: Action [FilePath]
+postNames = getDirectoryFiles "." ["site/posts//*.md"]
+
+-- | convert 'build' filepaths into source file filepaths
+destToSrc :: FilePath -> FilePath
+destToSrc p = "site" </> dropDirectory1 p
+
+-- | convert source filepaths into build filepaths
+srcToDest :: FilePath -> FilePath
+srcToDest p = "dist" </> dropDirectory1 p
+
+-- | convert a source file path into a URL
+srcToURL :: FilePath -> String
+srcToURL = ("/" ++) . dropDirectory1 . (-<.> ".html")
+
+--------------------------------------------------------------------------------
+-- Page Loaders
+
+loadEntity :: FromJSON b => EntityFilePath a -> Action b
+loadEntity (EntityFilePath path) = (subloadEntity path)
+
+subloadEntity :: FromJSON b => FilePath -> Action b
+subloadEntity entityPath = do
+  let srcPath = destToSrc entityPath -<.> "md"
+  entityData <- readFile' srcPath >>= myMarkdownToHTML . T.pack
+  let entityURL = T.pack  . srcToURL $ entityPath
+      withURL = _Object . at "url"     ?~ String entityURL
+      withSrc = _Object . at "srcPath" ?~ String (T.pack srcPath)
+  convert . withSrc . withURL $ entityData
+
+-- | Given a post source-file's file path as a cache key, load the Post object
+-- for it. This is used with 'jsonCache' to provide post caching.
+loadPost :: PostFilePath -> Action Post
+loadPost (PostFilePath postPath) = do
+  let srcPath = destToSrc postPath -<.> "md"
+  postData <- readFile' srcPath >>= markdownToHTML . T.pack
+  let postURL = T.pack . srcToURL $ postPath
+      withURL = _Object . at "url" ?~ String postURL
+      withSrc = _Object . at "srcPath" ?~ String (T.pack srcPath)
+  convert . withSrc . withURL $ postData
+
+--------------------------------------------------------------------------------
+-- Page Builders
+
+-- | given a cache of posts this will build a table of contents
+buildIndex :: (PostFilePath -> Action Post) -> FilePath -> Action ()
+buildIndex postCache out = do
+  posts <- postNames >>= traverse (postCache . PostFilePath)
+  indexT <- compileTemplate' "site/templates/index.html"
+  let indexInfo = IndexInfo {posts}
+      indexHTML = T.unpack $ substitute indexT (toJSON indexInfo)
+  writeFile' out indexHTML
+
+-- Build an html file for a given post given a cache of posts.
+buildPost :: (PostFilePath -> Action Post) -> FilePath -> Action ()
+buildPost postCache out = do
+  let srcPath = destToSrc out -<.> "md"
+      postURL = srcToURL srcPath
+  post <- postCache (PostFilePath srcPath)
+  template <- compileTemplate' "site/templates/post.html"
+  writeFile' out . T.unpack $ substitute template (toJSON post)
---
+++ example/app/Main.hs
@@ -0,0 +1,92 @@
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DuplicateRecordFields      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns             #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE RecordWildCards            #-}
+
+module Main where
+
+import           Control.Lens
+import           Data.Aeson                 as A
+import           Data.Aeson.Lens
+import           Data.Function              (on)
+import           Data.List                  (sortBy)
+import           Data.Map                   as M
+import           Data.Monoid
+import           Data.Set                   as S
+import qualified Data.Text                  as T
+import           Data.Text.Lens
+import           Data.Time
+import           Development.Shake
+import           Development.Shake.Classes
+import           Development.Shake.FilePath
+import           GHC.Generics               (Generic)
+import           Slick
+
+import           Builder
+import           Types
+
+--------------------------------------------------------------------------------
+
+-- | List our custom keys that we need to manage additional behaviour
+--
+data Flags =
+    Preview     -- ^ key to run Warp server
+  | Watch       -- ^ watch for changes
+  deriving (Eq)
+
+-- | Specific flags for Shake to allow custom keys required for specific logic
+--
+flags :: forall a. [OptDescr (Either a Flags)]
+flags =
+  [ Option "" ["preview"      ] (NoArg $ Right Preview    ) "running as preview"
+  , Option "" ["watch"        ] (NoArg $ Right Watch      ) "running with watch"
+  ]
+
+buildRules :: Foldable t => t Flags -> Rules ()
+buildRules flags = do
+  postCache <- jsonCache' loadPost
+
+  -- Require all the things we need to build the whole site
+  "site" ~> need ["static", "posts", "dist/index.html"]
+
+  -- Require all static assets
+  "static" ~> do
+    staticFiles <-
+      getDirectoryFiles "." ["site/css//*", "site/js//*", "site/images//*"]
+    need (("dist" </>) . dropDirectory1 <$> staticFiles)
+
+  -- Rule for handling static assets, just copy them from source to dest
+  ["dist/css//*", "dist/js//*", "dist/images//*"] |%> \out -> do
+    copyFileChanged ("site" </> dropDirectory1 out) out
+
+    -- Find and require every post to be built
+  "posts" ~> requirePosts
+
+  -- build the main table of contents
+  "dist/index.html" %> buildIndex postCache
+
+  -- rule for actually building posts
+  "dist/posts//*.html" %> buildPost postCache
+
+
+-- | Function to start custom Shake pipeline for execution
+--
+runSiteBuilder :: ShakeOptions                     -- ^ Options for the Shake builder
+               -> [OptDescr (Either String Flags)] -- ^ Converted CLI arguments
+               -> IO ()
+runSiteBuilder shOpts flags =
+ shakeArgsPruneWith
+    shOpts pruner flags $
+      \flags targets -> do
+        let rls = Just $ buildRules flags
+        return $ rls
+
+main :: IO ()
+main = do
+  shakeArgs <- getArgs
+  cwd       <- getCurrentDirectory
+  let shOpts  = shakeOptions {shakeVerbosity = Quiet}
+
+  runSiteBuilder shOpts flags
